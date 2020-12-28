c9f993128c50daa87f40637ec69a0eea8e1d5285
g-bolsoni <giovanebolsoni2@gmail.com>
2020-12-28 15:18:33 -0300

    Nav 0.04  Main 0.01

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    $toggleCollapse.click(function(){
+        $nav.$toggleClass('collapse')
+    })
+})
+
+
+
+
+
+
+/*$(document).ready(function(){
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /* click event on toggle menu
+    toggleCollapse.click(function(){
+        $nav.toggleClass('collapse')
+    })
+})*/
